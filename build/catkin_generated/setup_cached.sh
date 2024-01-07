2646977ee0436a6c2b7458fdcbbf96d0b0d134f3
sakuranekohon <114348771+sakuranekohon@users.noreply.github.com>
2024-01-07 15:38:46 +0800

    update

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,4 +7,9 @@
 # new environment variables
 
 # modified environment variables
+export CMAKE_PREFIX_PATH="/media/sf_IoT_final_project/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/media/sf_IoT_final_project/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/media/sf_IoT_final_project/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/media/sf_IoT_final_project/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/media/sf_IoT_final_project/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/media/sf_IoT_final_project/src:$ROS_PACKAGE_PATH"
